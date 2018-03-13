afb730d622da776dfe6661a211b7ca2559c4950c
Grom9941 <skv.41@ya.ru>
2018-03-13 03:48:38 +0300

    ВСЕ

---
+++ src/main/java/Infinity.java
@@ -0,0 +1,288 @@
+import java.util.*;
+
+public class Infinity implements Comparable<Infinity>{
+
+    private ArrayList<Byte> number = new ArrayList<>();
+
+    /**
+     * Преобразование в массив
+     */
+    public Infinity(String string) {
+        boolean zero = true;
+
+        for (int i = 0; i < string.length(); i++) {
+
+           // if (Character.isDigit(string.charAt(i))) throw new IllegalArgumentException("Не правильная запись числа");
+
+            if (string.charAt(i) != '0' || !zero) {
+                number.add((byte) Character.getNumericValue(string.charAt(i)));
+                zero = false;
+            }
+        }
+    }
+
+    /**
+     * Преобразование в строку
+     */
+    private String Main(ArrayList<Byte> array) {
+        boolean zero = true;
+        StringBuilder string = new StringBuilder();
+
+        for (Byte anArray : array) {
+
+            if (anArray != 0 || !zero) {
+                string.append(anArray);
+                zero = false;
+            }
+        }
+
+        return string.toString();
+    }
+
+    /**
+     * Добавление в начало массива 0
+     */
+    private ArrayList<Byte> Add0(int value, ArrayList<Byte> array) {
+
+        for (int i = 0; i < value; i++) {
+            array.add(0, (byte) 0);
+        }
+
+        return array;
+    }
+
+    /**
+     *Сложение
+     */
+    public String plus(Infinity number1) {
+
+        if (this.number.size() > number1.number.size()) {
+            number1.number = Add0(this.number.size() - number1.number.size(), number1.number);
+        } else this.number = Add0(number1.number.size() - this.number.size(), this.number);
+
+        int ost = 0;
+        int value;
+
+        for (int i = this.number.size() - 1; i >= 0; i--) {
+
+            value = (this.number.get(i) + number1.number.get(i) + ost) % 10;
+            ost = (this.number.get(i) + number1.number.get(i)) / 10;
+
+            this.number.remove(i);
+            this.number.add(i, (byte) value);
+        }
+
+        if (ost != 0) this.number.add(0, (byte) ost);
+        return Main(this.number);
+    }
+
+    /**
+     *Замена символа
+     */
+    private ArrayList<Byte> change(int i, ArrayList<Byte> array, int value) {
+
+        array.remove(i);
+        array.add(i, (byte) value);
+
+        return array;
+    }
+
+    /**
+     * Вычитание
+     */
+    public String minus(Infinity number1) {
+
+        int value;
+        int ost;
+        int compareNumber = new Infinity(this.number.toString()).compareTo(new Infinity(number1.number.toString()));
+
+        if (compareNumber == 0) return "0";
+        if (compareNumber < 0) throw new IllegalArgumentException("Отрицательное число");
+
+
+        number1.number = Add0(this.number.size() - number1.number.size(), number1.number);
+
+        for (int i = this.number.size() - 1; i >= 0; i--) {
+
+            if (this.number.get(i) >= number1.number.get(i)) {
+
+                value = this.number.get(i) - number1.number.get(i);
+
+                this.number = change(i, this.number, value);
+
+            } else {
+
+                value = 10 + this.number.get(i) - number1.number.get(i);
+                int j = i - 1;
+                ost = 9;
+                this.number = change(i, this.number, value);
+
+                while (this.number.get(j) == 0) {
+
+                    value = ost + this.number.get(j);
+                    this.number = change(j, this.number, value);
+                    j--;
+                }
+
+                value = this.number.get(j) - 1;
+                this.number = change(j, this.number, value);
+            }
+        }
+
+        return Main(this.number);
+    }
+
+
+
+    /**
+     * Умножение
+     */
+    public String multiplication(Infinity number1) {
+
+        ArrayList<Byte> intermediate = new ArrayList<>();
+        for (int i = 0; i < this.number.size()*number1.number.size()+1; i++) {
+            intermediate.add((byte) -1);
+        }
+
+        int compareNumber = new Infinity(this.number.toString()).compareTo(new Infinity(number1.number.toString()));
+        int value;
+        int placeNumber = 0;
+        int place = 0;
+        int getResult;
+
+        for (int i = number1.number.size() - 1; i >= 0; i--) {
+
+            place = placeNumber;
+            for (int j = this.number.size() - 1; j >= 0; j--) {
+
+                if (intermediate.get(place) == -1) getResult = 0;
+                else getResult = intermediate.get(place);
+
+                value = getResult + this.number.get(j) * number1.number.get(i);
+                intermediate = change(place, intermediate, value % 10);
+                place++;
+
+                if (value > 10) {
+                    intermediate = change(place, intermediate, value / 10);
+                }
+            }
+            placeNumber++;
+        }
+        ArrayList<Byte> result = new ArrayList<>();
+        while (place > 0) {
+            result.add(intermediate.get(place - 1));
+            place--;
+        }
+        return Main(result);
+    }
+
+    /**
+     * Деление (целая часть)
+     */
+    public long whole(Infinity number1) {
+
+        long whole = 0;
+        int compareNumber = new Infinity(this.number.toString()).compareTo(new Infinity(number1.number.toString()));
+
+        while (compareNumber > 0 || compareNumber == 0) {
+            this.number = new Infinity(new Infinity(Main(this.number)).minus(new Infinity(Main(number1.number)))).number;
+            whole++;
+            compareNumber = new Infinity(this.number.toString()).compareTo(new Infinity(number1.number.toString()));
+        }
+
+        return whole;
+    }
+
+    //Деление (остаток)
+    public String residue(Infinity number1) {
+
+        long whole = new Infinity(this.number.toString()).whole(new Infinity(number1.number.toString()));
+       
+        for (int i = 0; i < whole; i++){
+
+            this.number = new Infinity(new Infinity(Main(this.number)).minus(new Infinity(Main(number1.number)))).number;
+        }
+
+        if (this.number.size() == 0) return "0";
+        return Main(this.number);
+    }
+
+    //Максимальное
+    public String max(Infinity number1) {
+
+        int compareNumber = new Infinity(this.number.toString()).compareTo(new Infinity(number1.number.toString()));
+
+        if (compareNumber > 0) return Main(this.number);
+        if (compareNumber < 0) return Main(number1.number);
+
+        if (compareNumber < 0) {
+            ArrayList<Byte> changeArray = this.number;
+
+            this.number = number1.number;
+            number1.number = changeArray;
+        }
+        return "Одинаковые числа";
+    }
+
+    //Минимальное
+    public String min(Infinity number1) {
+
+        int compareNumber = new Infinity(this.number.toString()).compareTo(new Infinity(number1.number.toString()));
+
+        if (compareNumber > 0) return Main(number1.number);
+        if (compareNumber < 0) return Main(this.number);
+
+        return "Одинаковые числа";
+    }
+
+
+
+    @Override
+    public boolean equals(Object object) {
+
+        if (this == object) return true;
+
+        if (object instanceof Infinity) {
+
+            Infinity other = (Infinity) object;
+            return this.toString().equals(other.toString());
+        }
+
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+
+        final int prime = 31;
+
+        return number.hashCode() * prime;
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder string = new StringBuilder();
+        for (Byte digit : number) {
+
+                string.append(digit);
+        }
+
+        return string.toString();
+    }
+
+
+    @Override
+    public int compareTo(Infinity number1) {
+        if (this.number.size() > number1.number.size()) return 1;
+        if (this.number.size() < number1.number.size()) return -1;
+
+        for (int i = 0; i < this.number.size(); i++) {
+
+            if (this.number.get(i) > number1.number.get(i)) return 1;
+            if (this.number.get(i) < number1.number.get(i)) return -1;
+        }
+
+        return 0;
+    }
+}
--- src/test/java/TestNumber.java
+++ src/test/java/TestNumber.java
@@ -5,30 +5,29 @@ import static org.junit.Assert.*;
 public class TestNumber {
     @Test
     public void plus(){
-        Main number1 = new Main("222");
-        Main number2 = new Main("3");
-        Main number3 = new Main("1");
-        assertEquals("226",number1.plus(number2));
+        Infinity number1 = new Infinity("222");
+        Infinity number2 = new Infinity("3");
+        assertEquals("225",number1.plus(number2));
     }
 
     @Test
     public  void minus(){
-        Main number1 = new Main("77");
-        Main number2 = new Main("277");
-        assertEquals("-200",number1.minus(number2));
+        Infinity number1 = new Infinity("277");
+        Infinity number2 = new Infinity("77");
+        assertEquals("200",number1.minus(number2));
     }
 
     @Test
     public void max(){
-        Main number1 = new Main("11");
-        Main number2 = new Main("2");
+        Infinity number1 = new Infinity("11");
+        Infinity number2 = new Infinity("2");
         assertEquals("11",number1.max(number2));
     }
 
     @Test
     public void min(){
-        Main number1 = new Main("222");
-        Main number2 = new Main("122");
+        Infinity number1 = new Infinity("222");
+        Infinity number2 = new Infinity("122");
         assertEquals("122",number1.min(number2));
     //    try {
     //        new Main("!99");
@@ -38,29 +37,29 @@ public class TestNumber {
 
     @Test
     public void equals(){
-        Main number1 = new Main("222");
-        Main number2 = new Main("222");
+        Infinity number1 = new Infinity("222");
+        Infinity number2 = new Infinity("222");
         assertTrue(number1.equals(number2));
     }
 
     @Test
     public void multiplication(){
-        Main number1 = new Main("21");
-        Main number2 = new Main("100");
+        Infinity number1 = new Infinity("21");
+        Infinity number2 = new Infinity("100");
         assertEquals("2100",number1.multiplication(number2));
     }
 
     @Test
     public void whole(){
-        Main number1 = new Main("210");
-        Main number2 = new Main("100");
+        Infinity number1 = new Infinity("210");
+        Infinity number2 = new Infinity("100");
         assertEquals(2,number1.whole(number2));
     }
 
     @Test
     public void residue(){
-        Main number1 = new Main("100");
-        Main number2 = new Main("100");
+        Infinity number1 = new Infinity("100");
+        Infinity number2 = new Infinity("100");
         assertEquals("0",number1.residue(number2));
     }
 }
